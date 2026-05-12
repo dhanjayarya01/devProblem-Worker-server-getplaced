3383c7b351cda0a0c133970028d3bc2a295441f5
dhanjayarya <dhanjayarya20@gmail.com>
2026-05-12 10:38:42 +0530

    changed config to fixed the file

--- src/config.js
+++ src/config.js
@@ -4,9 +4,23 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+import fs from 'fs';
+
 // Make folder name configurable. Remote server uses 'getplaced-codearena', local uses 'codearenaproject'
 const projectsDir = process.env.PROJECTS_DIR_NAME || 'getplaced-codearena';
-export const PROJECTS_BASE_PATH = path.resolve(__dirname, '../../', projectsDir);
+
+// Dynamically try to find the correct projects base path
+let basePath = path.resolve(__dirname, '../../', projectsDir);
+if (fs.existsSync(path.resolve(__dirname, '../../codearenaproject'))) {
+    basePath = path.resolve(__dirname, '../../codearenaproject');
+} else if (fs.existsSync(path.resolve(__dirname, '../../getplaced-codearena'))) {
+    basePath = path.resolve(__dirname, '../../getplaced-codearena');
+} else if (fs.existsSync(path.resolve(__dirname, '../../../getplaced-codearena'))) {
+    // Handling the case where the repo was cloned inside a directory of the same name on the server
+    basePath = path.resolve(__dirname, '../../../getplaced-codearena');
+}
+
+export const PROJECTS_BASE_PATH = basePath;
 export const WORKSPACES_BASE_PATH = path.resolve(__dirname, '../../workspaces');
 export const CONTAINER_INTERNAL_PORT = 3000;
 export const PORT_RANGE_MIN = 4100;
